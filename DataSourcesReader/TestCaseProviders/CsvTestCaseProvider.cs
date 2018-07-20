7f9c6b7330a6464b20bc08f0614c86d75cb1bcaf
Mateusz Konicki <mateusz.konicki94@gmail.com>
2018-07-20 22:18:04 +0200

    Initialize implementation

---
+++ DataSourcesReader/TestCaseProviders/CsvTestCaseProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using DataSourcesReaders.Models;
+
+namespace DataSourcesReaders.TestCaseProviders
+{
+    public class CsvTestCaseProvider : ITestCaseProvider
+    {
+        public IEnumerable<dynamic> GetDynamic()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<T> GetGeneric<T>() where T : new()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<TestCase<TCase, TResult>> GetTestCases<TCase, TResult>()
+            where TCase : new()
+            where TResult : new()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
